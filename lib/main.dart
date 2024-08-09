ae7d52f131469713a8e097ea9bfd591441c11851
kefeh <kefehcollins@gmail.com>
2024-08-09 12:00:07 +0100

    adds routes management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:farmers_guide/app_theme.dart';
 import 'package:farmers_guide/login_ui.dart';
+import 'package:farmers_guide/services/user_state.dart';
 import 'package:farmers_guide/signup_ui.dart';
+import 'package:farmers_guide/weather_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,19 +20,33 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: appTheme,
-      onGenerateRoute: (RouteSettings routeSettings) {
-        return MaterialPageRoute<void>(
-          settings: routeSettings,
-          builder: (BuildContext context) {
-            switch (routeSettings.name) {
-              case LoginUi.routeName:
-                return const LoginUi();
-              default:
-                return const SignUpUi();
-            }
-          },
-        );
+      routes: {
+        WeatherUi.routeName: (_) => const WeatherUi(),
+        LoginUi.routeName: (_) => const LoginUi(),
+        SignUpUi.routeName: (_) => const SignUpUi(),
       },
+      home: FutureBuilder(
+        future: userMeState.initialise(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: SizedBox.square(
+                dimension: 60,
+                child: LoadingIndicator(
+                  indicatorType: Indicator.lineScale,
+                  colors: [Colors.black],
+                ),
+              ),
+            );
+          }
+          final user = userMeState.value;
+          if (user == null) {
+            return const LoginUi();
+          } else {
+            return const WeatherUi();
+          }
+        },
+      ),
     );
   }
 }
