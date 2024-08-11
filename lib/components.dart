9415583a72abaa2ce2fce13deaf68c7d433a0d9b
kefeh <kefehcollins@gmail.com>
2024-08-11 16:05:49 +0100

    adds componenets for advice and other refactors

--- lib/components.dart
+++ lib/components.dart
@@ -1,4 +1,6 @@
+import 'package:farmers_guide/models/crop_advice.dart';
 import 'package:farmers_guide/services/providers.dart';
+import 'package:farmers_guide/ui/login_ui.dart';
 import 'package:farmers_guide/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -74,58 +76,114 @@ class CurrentEssentialWeatherCard extends ConsumerWidget {
                 // ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: () {
-                ref.watch(expandProvider.notifier).update((state) => !state);
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Perfect weather for planting",
+            const SizedBox(height: 12),
+            ref.watch(selectedCrop) == null
+                ? Text(
+                    "No crop selected",
                     style: Theme.of(context).textTheme.titleLarge!.copyWith(
                           color: Colors.white,
                         ),
-                  ),
-                  Icon(
-                    ref.watch(expandProvider)
-                        ? CupertinoIcons.chevron_up_circle
-                        : CupertinoIcons.chevron_down_circle,
-                    color: Colors.white,
                   )
-                ],
-              ),
-            ),
-            Expanded(
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                switchInCurve: Curves.easeIn,
-                switchOutCurve: Curves.easeOut,
-                child: ref.watch(expandProvider)
-                    ? ListView(
-                        children: const [
-                          MoreItem(
-                              message:
-                                  "Moderate Temperatures: Cool to mild temperatures are best, generally found in spring and fall."),
-                          MoreItem(
-                              message:
-                                  "Moderate Temperatures: Cool to mild temperatures are best, generally found in spring and fall."),
-                          MoreItem(
-                              message:
-                                  "Moderate Temperatures: Cool to mild temperatures are best, generally found in spring and fall."),
-                          MoreItem(
-                              message:
-                                  "Moderate Temperatures: Cool to mild temperatures are best, generally found in spring and fall."),
-                        ],
-                      )
-                    : const SizedBox.shrink(),
-              ),
-            )
+                : GestureDetector(
+                    onTap: () {
+                      ref
+                          .watch(expandProvider.notifier)
+                          .update((state) => !state);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "See insights for ${ref.watch(selectedCrop)?.cropType}",
+                          style:
+                              Theme.of(context).textTheme.titleMedium!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                        Icon(
+                          ref.watch(expandProvider)
+                              ? CupertinoIcons.chevron_up_circle
+                              : CupertinoIcons.chevron_down_circle,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+            const Expanded(child: AdviceWidget())
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AdviceWidget extends ConsumerWidget {
+  const AdviceWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final crop = ref.watch(selectedCrop);
+    final weather = ref.watch(selectedForcast);
+    if (weather == null || crop == null) {
+      return Container();
+    }
+    final forcastDate = weather.date.split("T")[0];
+    final cropAdviceAsync =
+        ref.watch(getCropAdviceProvider(crop.id, forcastDate));
+
+    return cropAdviceAsync.map(
+      data: (data) {
+        final CropAdvice? advice = data.value.$2;
+        final error = data.value.$1;
+        if (error != null) {
+          return Text(
+            error,
+            style: const TextStyle(color: Colors.white),
+          );
+        }
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 500),
+          switchInCurve: Curves.easeIn,
+          switchOutCurve: Curves.easeOut,
+          child: ref.watch(expandProvider)
+              ? ListView(
+                  children: [
+                    MoreItem(
+                      title: 'Advice type:',
+                      message: advice!.adviceType,
+                    ),
+                    MoreItem(
+                      title: 'Insight:',
+                      message: advice.advice,
+                    ),
+                    MoreItem(
+                      title: 'Other things to note:',
+                      message: advice.otherThingsToNote,
+                    ),
+                    MoreItem(
+                      title: 'How long is advice valide:',
+                      message: advice.duration,
+                    ),
+                    MoreItem(
+                      title: 'Confidence level:',
+                      message: advice.confidenceLevel.toString() == '-1.0'
+                          ? 'N/A'
+                          : advice.confidenceLevel.toString(),
+                    ),
+                  ],
+                )
+              : const SizedBox.shrink(),
+        );
+      },
+      error: (_) => const Text(
+        "Error fetching insights",
+        style: TextStyle(color: Colors.white),
+      ),
+      loading: (_) => const Text(
+        "Fetching insights...",
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
@@ -135,17 +193,38 @@ class MoreItem extends StatelessWidget {
   const MoreItem({
     super.key,
     required this.message,
+    required this.title,
+    this.isDark = false,
   });
 
   final String message;
+  final String title;
+  final bool isDark;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
-      child: Text(
-        message,
-        style: const TextStyle(color: Colors.white),
+      child: RichText(
+        softWrap: true,
+        text: TextSpan(
+          text: '$title ',
+          style: DefaultTextStyle.of(context).style.copyWith(
+                fontWeight: FontWeight.bold,
+                color: isDark ? null : Colors.white,
+                fontSize: isDark ? 18 : 16,
+              ),
+          children: <TextSpan>[
+            TextSpan(
+              text: message,
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: isDark ? null : Colors.white,
+                fontSize: isDark ? 16 : 14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
